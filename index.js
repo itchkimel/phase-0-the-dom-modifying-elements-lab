1e478196193daa1fa0702cf6d0cc82befc2cab73
Yitzchok Kimelman <itchkimel@gmail.com>
2021-03-16 16:23:01 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.querySelector('main').remove();
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+const victiryId = newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = 'Itche is the champion';
